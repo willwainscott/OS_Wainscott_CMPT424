e94bf5857dd95e9b23f57447b3d7615368b689b0
Will Wainscott <willwainscott@gmail.com>
2019-10-30 15:14:27 -0400

    Modified Memory   #closes 23
    Made memory 3 times as big! Also added base and limit registers to the memory and functions to return the correct value based on a given section.

--- distrib/host/memory.js
+++ distrib/host/memory.js
@@ -2,12 +2,48 @@ var TSOS;
 (function (TSOS) {
     var Memory = /** @class */ (function () {
         function Memory() {
-            this.memoryArray = new Array(256);
+            this.memoryArray = new Array(768);
+            this.sectionOneBase = 0;
+            this.sectionOneLimit = 255;
+            this.sectionTwoBase = 256;
+            this.sectionTwoLimit = 511;
+            this.sectionThreeBase = 512;
+            this.sectionThreeLimit = 767;
         }
         Memory.prototype.init = function () {
             // initialize the memory as all "00"
             for (var i = 0; i < this.memoryArray.length; i++) {
                 this.memoryArray[i] = "00";
+            }
+        };
+        Memory.prototype.getBaseBySection = function (section) {
+            switch (section) {
+                case "1":
+                    return this.sectionOneBase;
+                    break;
+                case "2":
+                    return this.sectionTwoBase;
+                    break;
+                case "3":
+                    return this.sectionThreeBase;
+                    break;
+                default:
+                    console.log("Invalid section number when calling Memory.getBaseBySection()");
+            }
+        };
+        Memory.prototype.getLimitBySection = function (section) {
+            switch (section) {
+                case "1":
+                    return this.sectionOneLimit;
+                    break;
+                case "2":
+                    return this.sectionTwoLimit;
+                    break;
+                case "3":
+                    return this.sectionThreeLimit;
+                    break;
+                default:
+                    console.log("Invalid section number when calling Memory.getLimitBySection()");
             }
         };
         return Memory;
